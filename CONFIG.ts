8cff80b630cbb81f69292c495c4c02610cb6a54f
UGamerZ <gasterdreemyr@gmail.com>
2026-04-09 16:02:21 +0800

    upd creds

--- CONFIG.ts
+++ CONFIG.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
     GOOGLE_CLIENT_ID: "495784182640-719c3pge6qaas4blds2v7jqtiga1dsue.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
     GOOGLE_REDIRECT_URI: "http://localhost:3000",
-    GOOGLE_REFRESH_TOKEN: "1//03lCj5YVBDAejCgYIARAAGAMSNwF-L9IrZSuoLiIo15ZrNiOCClVaPPsV7GW9ZJkURP7pqv66bLuZWohAofz-esuZe_ACxXB01fs",
-    GOOGLE_DRIVE_FOLDER_ID: "12nLVASiMaz7iBHgsHSz52mk3BYSkjQEJ",
+    GOOGLE_REFRESH_TOKEN: "1//0cEf9Bw-Sx9iCCgYIARAAGAwSNwF-L9Ir2wg-7a4AzxQqhd__hKLv8Pk7ipY4l6Zro9Q6sjbMliGwnOn9kWaYb2Yxh3Dk6PPRvzk",
+    GOOGLE_DRIVE_FOLDER_ID: "18uPreUDv3WZrvl7zZFrmbQUSRFei-JCp",
     LOCAL_IP: "https://protruck38storagebackend.onrender.com"
 }
